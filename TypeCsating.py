1a34f9443b732b8da74ad30925444e1361b4f7ed
vireetha <dudalaviree69@gmail.com>
2025-01-02 06:02:44 +0530

    type casting

---
+++ TypeCsating.py
@@ -0,0 +1,19 @@
+name = "Penguin"
+age = 15
+is_student = True
+weight = 38.5
+print("Name : ", name)
+print("Data type of Name is:",type(name))
+print("Age: ",age)
+print("Data type of Age is: ",type(age))
+print("is_student: ",is_student)
+print("Data type of ab id : ",type(is_student))
+print("weight: ",weight)
+print("Data type of weight is: ", type(weight))
+#Type Casting
+print("\n After Type Casting")
+age = str(age)
+print(age)
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is",type(weight))
