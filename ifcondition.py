f9374650f9490fe80ce9d566295d89f738b988bd
vireetha <dudalaviree69@gmail.com>
2025-01-10 05:42:11 +0530

    ifcondition

---
+++ ifcondition.py
@@ -0,0 +1,10 @@
+answer = input("When was the first known use of the word 'quiz' ?")
+if answer == "1781":
+    print("correct")
+else:
+    print("The answer is '1781' , not",answer,"!!")
+answer = input("Which built-in function can get information from the user ?")
+if answer == "input":
+    print('Correct')
+else:
+    print("The answer is 'input' , not",answer,"...")
